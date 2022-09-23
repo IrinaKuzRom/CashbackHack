a979eaf30a4f666977ad405a8ecb3e1f24eeebd2
Irina <kuznetsova.ir.bog@gmail.com>
2022-09-23 21:18:52 +0300

    added src and build.gradle

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+@Test
+public void testOnNull() {
+    CashbackHackService cashback=new CashbackHackService();
+    int amount=1000;
+    int actual=cashback.remain(amount);
+    int expected=1000;
+    assertEquals(expected,actual);
+}
+    @Test
+    public void testOnHundred() {
+        CashbackHackService cashback=new CashbackHackService();
+        int amount=900;
+        int actual=cashback.remain(amount);
+        int expected=100;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testOnZero() {
+        CashbackHackService cashback=new CashbackHackService();
+        int amount=0;
+        int actual=cashback.remain(amount);
+        int expected=1000;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testOnTwo() {
+        CashbackHackService cashback=new CashbackHackService();
+        int amount=2500;
+        int actual=cashback.remain(amount);
+        int expected=500;
+        assertEquals(expected,actual);
+    }
+}
